bfe019144be4205daf7934038884bb89a2d6f35b
MATpoliteLIB <145462284+MATpoliteLIB@users.noreply.github.com>
2023-11-21 16:20:34 +0300

    Add files via upload

---
+++ albedo/main.py
@@ -0,0 +1,10 @@
+import script as f
+from time import sleep
+from picamera import PiCamera
+
+camera = PiCamera()
+camera.resolution = (1024, 768)
+camera.start_preview()
+# Camera warm-up time
+sleep(2)
+camera.capture('HeatYellow1.jpg')
